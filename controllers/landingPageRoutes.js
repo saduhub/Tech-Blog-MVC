d8ceb2cc67221a0abf474c634247b04255fa45a5
Jorge Duarte <sadujorge@gmail.com>
2023-07-09 21:50:49 -0700

    finished making and conecting most of the views

--- controllers/landingPageRoutes.js
+++ controllers/landingPageRoutes.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
 router.get('/home', (req, res) => {
     res.render('home');
 });
+// Define dashboard page route. Add middleware to check if user is logged in. Render all user posts
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
 // Define Login route that wil take user input to login and render user dashboard if successful
 
 // Define signup route that will take user input to signup and render user dashboard if successful
---
+++ public/js/hamburger.js
@@ -0,0 +1,80 @@
+// Mobile nav bar open and close
+let hamburgerButton = $('#hamburger');
+let navContent = $('#nav-content');
+
+hamburgerButton.on('click', function() {
+  if (navContent.is(':hidden')) {
+    navContent.slideDown();
+  } else {
+    navContent.slideUp();
+  }
+});
+
+
+
+// // Event listener for each tile
+// $(".tile").on("click", function () {
+//   searchQuery = $(this).text();
+//   getInfo();
+//   console.log(searchQuery);
+//   console.log(artistDescription);
+//   $('#picks-main').remove();
+//   $('#description').remove();
+// // will only work if artist is removed from array
+//   if (artistArray.includes(searchQuery)) {
+//     return;
+//   } else {
+//     artistArray.push(searchQuery);
+//     localStorage.setItem("artists", artistArray);
+//     artistString = localStorage.getItem("artists");
+//     artistArray = artistString.split(",")
+//     console.log(artistArray)
+//     generateArtistButtons();
+//   }
+// });
+
+// // Navigation to home
+// let homeBtn = $("#home");
+
+// homeBtn.on("click", function () {
+//   location.reload();
+// });
+
+
+
+
+
+// // input
+// $("#search-button1").on("click", function() {
+//   $('#description').addClass('hidden');
+//   $('#picks-main').addClass('hidden');
+//   $('#artist-profile').addClass('hidden');
+//   $('#video-div').removeClass('hidden');
+//   if (navContent.is(':hidden')) {
+//     navContent.slideDown();
+//   } else {
+//     navContent.slideUp();
+//   }
+
+// });
+
+// $("#search-button2").on("click", function() {
+//   $('#description').addClass('hidden');
+//   $('#picks-main').addClass('hidden');
+//   $('#artist-profile').addClass('hidden');
+//   $('#video-div').removeClass('hidden');
+//   if (navContent.is(':hidden')) {
+//     navContent.slideDown();
+//   } else {
+//     navContent.slideUp();
+//   }
+// });
+
+
+// document.querySelectorAll(".tile").forEach((item) => {
+//   item.addEventListener("click", () => {
+//     let searchQuery = $(item).text();
+    
+//   });
+
+// });
