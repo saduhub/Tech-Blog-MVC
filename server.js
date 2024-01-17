da3049b051e6d008e9e116e201b1237fe340aa6b
Jorge Duarte <sadujorge@gmail.com>
2024-01-16 21:05:30 -0800

    Re-arranged server.js, added session configuration, routing, and dotenv to server.js

--- server.js
+++ server.js
@@ -1,53 +1,54 @@
-// Import path.
+// Import necessary modules
+require('dotenv').config();
 const path = require('path');
-// Import Express.
 const express = require('express');
 const session = require('express-session');
-// Import handlebars.
 const exphbs = require('express-handlebars');
-// Import controllers.
 const routes = require('./controllers');
-// Import connection to db via Sequelize and mysql2 driver.
 const sequelize = require('./config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// Create new instance of express.
-const app = express();
-// Set post where server will listen.
-const PORT = process.env.PORT || 3001;
-// Set up a view engine for application. Needed when using express-handlebars 7.0.7
-app.engine('handlebars', exphbs.engine());
-// Set the view engine value to habdlebars.
-app.set('view engine', 'handlebars');
-
+// Configure session storage
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SECRET, 
   cookie: {
     maxAge: 300000,
-    httpOnly: true,
+    httpOnly: true, 
     secure: false,
-    sameSite: 'strict',
+    sameSite: 'strict', 
   },
-  resave: false,
-  saveUninitialized: true,
+  resave: false, 
+  saveUninitialized: true, 
   store: new SequelizeStore({
-    db: sequelize
+    db: sequelize 
   })
 };
 
+// Initialize Express app
+const app = express();
+
+// Configure server port
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars as the view engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// Allow json parsing
+app.use(express.json());
+// Allow URL parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Create absolute path to serve static assets in public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Apply session configuration to app
 app.use(session(sess));
 
-// Allow json parsing.
-app.use(express.json());
-// Allow URL parsing.
-app.use(express.urlencoded({ extended: true }));
-// Create absolute path to serve static assets in public folder.
-app.use(express.static(path.join(__dirname, 'public')));
-// Make use of controllers when listening for user input.
+// Set up application routes
 app.use(routes);
 
-// Implement Sequlize to sync models to databse and start server.
+// Implement Sequlize to sync models to database and start server
 sequelize.sync({ force: false }).then(() => {
   // Set up server at specified port
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
